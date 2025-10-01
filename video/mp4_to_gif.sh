64c0073c6dfbcda7d9f220311db520543e315ecd
Danny Blaker <noemail@nothing.com>
2025-10-01 22:43:11 +1000

    mp4 to gif

--- video/mp4_to_gif.sh
+++ video/mp4_to_gif.sh
@@ -3,3 +3,5 @@ ffmpeg -y -i video.mp4 \
  [v2]palettegen=max_colors=256:stats_mode=full[p]; \
  [v1][p]paletteuse=dither=sierra2_4a:diff_mode=rectangle" \
 -r 30 -loop 0 output.gif
+
+# convert mp4 video to a high quality gif
