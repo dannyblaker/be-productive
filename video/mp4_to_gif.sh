1489d294678a49c03f168872e345b267563162d3
Danny Blaker <noemail@nothing.com>
2025-10-01 22:42:34 +1000

    mp4 to gif

---
+++ video/mp4_to_gif.sh
@@ -0,0 +1,5 @@
+ffmpeg -y -i video.mp4 \
+-filter_complex "[0:v]split[v1][v2]; \
+ [v2]palettegen=max_colors=256:stats_mode=full[p]; \
+ [v1][p]paletteuse=dither=sierra2_4a:diff_mode=rectangle" \
+-r 30 -loop 0 output.gif
