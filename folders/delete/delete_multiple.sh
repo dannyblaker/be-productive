415b2c5e19b127af9f00a2680fdd4d3c12e16415
Danny Blaker <noemail@nothing.com>
2025-10-03 22:22:59 +1000

    fix: update delete_multiple.sh to include .pytest_cache directory

--- folders/delete/delete_multiple.sh
+++ folders/delete/delete_multiple.sh
@@ -1,1 +1,1 @@
-find . \( -type d -name ".conda" -o -name ".venv" -o -name "node_modules" -o -name "pycache" \) -exec rm -rf {} +
+find . \( -type d -name ".conda" -o -name ".venv" -o -name "node_modules" -o -name "__pycache__" -o -name ".pytest_cache" \) -exec rm -rf {} +
