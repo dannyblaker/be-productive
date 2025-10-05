66418cd90f094b9e9819918e064d9e382216bccf
Danny Blaker <noemail@nothing.com>
2025-10-05 22:55:03 +1100

    refactor: simplify delete_empty_dirs.sh

--- folders/delete/delete_empty_dirs.sh
+++ folders/delete/delete_empty_dirs.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
+while true; do output=$(sudo find . -type d -empty -exec rmdir {} \; 2>&1); echo "$output"; if [ -z "$output" ]; then echo "Output is empty."; break; fi done
 
-# delete empty directories in current directory and subdirectories
-
-while true; do
-    output=$(sudo find . -type d -empty -exec rmdir {} \; 2>&1)
-    echo "$output"
-    if [ -z "$output" ]; then
-        echo "Output is empty."
-        break
-    fi
-done
+# delete empty directories recursively starting from current directory
--- folders/delete/delete_empty_dirs_one_liner.sh
+++
@@ -1,3 +0,0 @@
-while true; do output=$(sudo find . -type d -empty -exec rmdir {} \; 2>&1); echo "$output"; if [ -z "$output" ]; then echo "Output is empty."; break; fi done
-
-# delete empty directories recursively starting from current directory
